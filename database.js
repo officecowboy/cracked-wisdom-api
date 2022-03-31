f6f2f27e948fd5aae3532c8ba1260a736c048e58
Scott Diekema <scott.diekema@gmail.com>
2022-03-31 16:31:13 -0400

    seed function successful

--- database.js
+++ database.js
@@ -10,3 +10,6 @@ mongoose.connect(`mongodb://127.0.0.1:27017/${DATABASE_NAME}`, mongooseConfig)
 mongoose.connection.on('connected', () => console.log("Connected to database"))
 mongoose.connection.on('disconnected', () => console.log("Disconnected from database"))
 mongoose.connection.on('error', error => console.error("Database error", error))
+
+// Export the connection
+module.exports = mongoose
--- seed/data.js
+++ seed/data.js
@@ -1,13 +1,15 @@
+let database = require('../database')
+
 // Import quotes data
 let quotes = require('./quotes.json')
 
 // Import Quote model
-let Quote = require('../server/quotes/model.js')
+let Quote = require('../server/quotes/model')
 
 Quote.deleteMany({})
   .then(() => {
     Quote.create(quotes)
       .then(() => {
-        mongoose.disconnect()
+        database.disconnect()
       })
   })
