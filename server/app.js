2dda9a636b2cbd7f1bca5a5a573e039427da2758
Scott Diekema <scott.diekema@gmail.com>
2022-04-25 09:39:25 -0400

    add cors

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
 let express = require('express')
 let router = require('./router')
+let cors = require('cors')
 
 let app = express()
 
 app.use(express.json())
+app.use(cors())
 app.use(router)
 
 module.exports = app
